502b22f6928285bf44766c572191d4fb5f08c343
M. Eduarda <beleski.maria@escola.pr.gov.br>
2026-02-23 10:54:30 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,24 @@
+nome = "Maria Eduarda"
+idade = 16
+altura = 1.68
+genero = "feminino"
+cor_de_pele = "parda"
+cor_de_cabelo = "rosa"
+tamanho_cabelo = "médio"
+estilo_cabelo = "ondulado com franja"
+hobby = "desenhar e dormir"
+
+# Novas variáveis
+curso = "Desenvolvimento de Sistemas"
+hobby_favorito = "desenhar"
+
+# Autodescrição
+print("Olá, meu nome é", nome + 
+      ", tenho", idade, "anos e", altura, "m de altura.",
+      "Sou do gênero", genero + 
+      ", minha cor de pele é", cor_de_pele + 
+      ", meu cabelo é", cor_de_cabelo + 
+      ",", tamanho_cabelo, "e do estilo", estilo_cabelo + ".",
+      "Atualmente faço o curso de", curso + 
+      "e meu hobby é", hobby + 
+      ", sendo que meu hobby favorito é", hobby_favorito + ".")
