94e6efa44226a45cf352b8aedb0d4e517a0f9b1f
M. Eduarda <beleski.maria@escola.pr.gov.br>
2026-02-23 10:55:29 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -13,12 +13,12 @@ curso = "Desenvolvimento de Sistemas"
 hobby_favorito = "desenhar"
 
 # Autodescrição
-print("Olá, meu nome é", nome + 
+print("Olá, meu nome é", nome, 
       ", tenho", idade, "anos e", altura, "m de altura.",
-      "Sou do gênero", genero + 
-      ", minha cor de pele é", cor_de_pele + 
-      ", meu cabelo é", cor_de_cabelo + 
-      ",", tamanho_cabelo, "e do estilo", estilo_cabelo + ".",
-      "Atualmente faço o curso de", curso + 
-      "e meu hobby é", hobby + 
-      ", sendo que meu hobby favorito é", hobby_favorito + ".")
+      "Sou do gênero", genero,
+      ", minha cor de pele é", cor_de_pele, 
+      ", meu cabelo é", cor_de_cabelo,
+      ",", tamanho_cabelo, "e do estilo", estilo_cabelo, ".",
+      "Atualmente faço o curso de", curso, 
+      "e meu hobby é", hobby, 
+      ", sendo que meu hobby favorito é", hobby_favorito, ".")
